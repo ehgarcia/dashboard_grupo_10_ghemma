d69d7df7417e165642ffd01c09558c3064762b50
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2023-01-09 19:29:24 -0300

    fix cors error

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -4,11 +4,13 @@ const User = db.User;
 const controller = {
   listAll: async function (req, res) {
     const users = await User.findAll();
+    res.set('Access-Control-Allow-Origin', '*');
     res.json(users);
   },
   listDetailed: async function (req, res){
     const { id } = req.params
     const user = await User.findOne({where: {id} })
+    res.set('Access-Control-Allow-Origin', '*');
     res.json(user)
   }
 };
