9277dcc62ce6987f9ddf3ad80b384a328c0752ef
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2023-01-17 21:08:27 -0300

    correcciones en config

--- frontend/src/components/LastProduct.jsx
+++ frontend/src/components/LastProduct.jsx
@@ -26,7 +26,7 @@ export function LastProduct() {
                             </div>
                             <div className="card-body">
                                 <div className="text-center">
-                                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: `40rem`}} src={arrayProduct[arrayProduct.length -1].image} alt=" Star Wars - Mandalorian " />
+                                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: `40rem`}} src={arrayProduct[arrayProduct.length -1].image} alt={`${arrayProduct[arrayProduct.length -1].name}`} />
                                 </div>
                                 <a className="btn btn-danger" rel="nofollow" href={`http://localhost:4000/api/products/${arrayProduct[arrayProduct.length -1].id}`}>View Device detail</a>
                             </div>
